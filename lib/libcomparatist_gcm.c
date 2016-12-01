f163aabef5a6b3e1f6ed5dd9257d5231c6eb1b83
Takafumi Arakaki <aka.tkf@gmail.com>
2016-12-01 15:47:48 -0800

    Use restrict keyword in libcomparatist_gcm.c

--- lib/libcomparatist_gcm.c
+++ lib/libcomparatist_gcm.c
@@ -12,43 +12,71 @@ inline double average(int dim, double * x){
   return tmp / dim;
 }
 
-inline double logistic(GloballyCoupledMap *self, double x) {
-  return self->r * x * (1 - x);
+inline double logistic(double r, double x) {
+  return r * x * (1 - x);
 }
 
-inline double tent(GloballyCoupledMap *self, double x) {
-  if (x < 1 / self->r) {
-    return self->r * x;
+inline double tent(double r, double x) {
+  if (x < 1 / r) {
+    return r * x;
   } else {
-    return self->r * (x - 1) / (1 - self->r);
+    return r * (x - 1) / (1 - r);
+  }
+}
+
+inline void GloballyCoupledMap_logistic_evolve(
+    int dim,
+    double x0[restrict dim],
+    double x1[restrict dim],
+    double mean,
+    double epsilon,
+    double r){
+  int i;
+  for (i = 0; i < dim; ++i){
+    x1[i] = logistic(r, (1 - epsilon) * x0[i] + epsilon * mean);
+  }
+}
+
+inline void GloballyCoupledMap_tent_evolve(
+    int dim,
+    double x0[restrict dim],
+    double x1[restrict dim],
+    double mean,
+    double epsilon,
+    double r){
+  int i;
+  for (i = 0; i < dim; ++i){
+    x1[i] = tent(r, (1 - epsilon) * x0[i] + epsilon * mean);
   }
 }
 
 void GloballyCoupledMap_logistic(GloballyCoupledMap *self){
-  int t, i;
+  int t;
   double mean;
   for (t = 1; t < self->steps; ++t){
     mean = average(self->dim, self->x + self->dim * (t - 1));
-    for (i = 0; i < self->dim; ++i){
-      self->x[self->dim * t + i] = logistic(
-        self,
-        (1 - self->epsilon) * self->x[self->dim * (t - 1) + i]
-        + self->epsilon * mean);
-    }
+    GloballyCoupledMap_logistic_evolve(
+      self->dim,
+      self->x + self->dim * (t - 1),
+      self->x + self->dim * t,
+      mean,
+      self->epsilon,
+      self->r);
   }
 }
 
 void GloballyCoupledMap_tent(GloballyCoupledMap *self){
-  int t, i;
+  int t;
   double mean;
   for (t = 1; t < self->steps; ++t){
     mean = average(self->dim, self->x + self->dim * (t - 1));
-    for (i = 0; i < self->dim; ++i){
-      self->x[self->dim * t + i] = tent(
-        self,
-        (1 - self->epsilon) * self->x[self->dim * (t - 1) + i]
-        + self->epsilon * mean);
-    }
+    GloballyCoupledMap_tent_evolve(
+      self->dim,
+      self->x + self->dim * (t - 1),
+      self->x + self->dim * t,
+      mean,
+      self->epsilon,
+      self->r);
   }
 }
 
